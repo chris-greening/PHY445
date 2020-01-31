e7f84b39b0f6fb38d3690e664167cf1084898d27
Chris <chrisgreening3@gmail.com>
2020-01-31 10:29:15 -0500

    looking at some fitting demo's

---
+++ Brainstorming/fitting_demo.py
@@ -0,0 +1,35 @@
+#Author: Chris Greening 
+#Date: 1/31/2020
+#Purpose: Messing around with fitting data, code taken from 
+#Source: http://scipyscriptrepo.com/wp/?p=104
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+def fitFunc(t, a, b, c):
+    return a*np.exp(-b*t) + c
+
+t = np.linspace(0,4,50)
+temp = fitFunc(t, 2.5, 1.3, 0.5)
+noisy = temp + 0.25*np.random.normal(size=len(temp))
+fitParams, fitCovariances = curve_fit(fitFunc, t, noisy)
+print(fitParams)
+print(fitCovariances)
+
+plt.ylabel('Temperature (C)', fontsize = 16)
+plt.xlabel('time (s)', fontsize = 16)
+plt.xlim(0,4.1)
+# plot the data as red circles with errorbars in the vertical direction
+plt.errorbar(t, noisy, fmt = 'ro', yerr = 0.2)
+# now plot the best fit curve and also +- 3 sigma curves
+# the square root of the diagonal covariance matrix element 
+# is the uncertianty on the corresponding fit parameter.
+sigma = [fitCovariances[0,0], fitCovariances[1,1], fitCovariances[2,2] ]
+plt.plot(t, fitFunc(t, fitParams[0], fitParams[1], fitParams[2]),\
+         t, fitFunc(t, fitParams[0] + sigma[0], fitParams[1] - sigma[1], fitParams[2] + sigma[2]),\
+         t, fitFunc(t, fitParams[0] - sigma[0], fitParams[1] + sigma[1], fitParams[2] - sigma[2])\
+         )
+plt.show()
+# save plot to a file
+# savefig('dataFitted.pdf', bbox_inches=0, dpi=600)
--- Brainstorming/theoretical_tools.py
+++ Brainstorming/theoretical_tools.py
@@ -6,7 +6,7 @@ from functools import partial
 from typing import List
 
 import matplotlib.pyplot as plt 
-from numpy import arange 
+from numpy import arange, sin, exp 
 
 def theoretical(
         func,
@@ -26,20 +26,33 @@ def theoretical(
 
 if __name__ == '__main__':
    
-    def quadratic(a, b, c, x):
-       """Model of quadratic function"""
+    def quadratic_example():
+        def quadratic(a, b, c, x):
+            """Model of quadratic function"""
 
-       return a*x**2 + b*x + c
-    
-    #bind a,b,c values 
-    quad_func = partial(quadratic, 1, 2, 1)
+            return a*x**2 + b*x + c
+            
+            #bind a,b,c values 
+            quad_func = partial(quadratic, 1, 2, 1)
 
-    #calculate data 
-    data = theoretical(quad_func, -50, 50,.1)
-    
-    #unpack tuple of data 
-    x,y = data
+            #calculate data 
+            data = theoretical(quad_func, -50, 50,.1)
+            
+            #unpack tuple of data 
+            x,y = data
 
-    #plot
-    plt.plot(x,y)
-    plt.show()
+            #plot
+            plt.plot(x,y)
+            plt.show()
+
+    def damped_oscillator_example():
+        def damped_oscillator(a, b, t):
+            return exp(-t/a)*sin(b*t)
+
+        osc_func = partial(damped_oscillator, 3, 3)
+        data = theoretical(osc_func, 0, 20, .1)
+        x,y = data 
+        plt.plot(x,y)
+        plt.show()
+
+    damped_oscillator_example()
