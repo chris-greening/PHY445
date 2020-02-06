348cd6dfc3cc64b6df21b7c7e35d35c4533b9b88
Chris <chrisgreening3@gmail.com>
2020-02-06 10:53:30 -0500

    equations for hall effect

---
+++ Hall Effect/equations.py
@@ -0,0 +1,11 @@
+#Author: Chris Greening 
+#Date: 2/6/2020
+#Purpose: Equations for the Hall effect lab 
+
+import numpy as np 
+from scipy.constants import physical_constants
+
+
+
+if __name__ == '__main__': 
+    pass 
