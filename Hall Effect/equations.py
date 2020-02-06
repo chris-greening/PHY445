4450c49f13a2d5ef329caab1857a70c27802d048
Chris <chrisgreening3@gmail.com>
2020-02-06 12:08:13 -0500

    carrier mobility as function R_H and R_square

--- Hall Effect/equations.py
+++ Hall Effect/equations.py
@@ -1,11 +1,22 @@
 #Author: Chris Greening 
 #Date: 2/6/2020
-#Purpose: Equations for the Hall effect lab 
+#Purpose: Theoretical equations for the Hall effect lab 
 
 import numpy as np 
 from scipy.constants import physical_constants
 
-
+def mobility(R_h, R_square):
+    """Calculate mobility from Hall resistance and Resistance per unit square"""
+    return R_h/R_square 
 
 if __name__ == '__main__': 
-    pass 
+    import sys 
+    sys.path.append('../Brainstorming')
+    from functools import partial 
+    
+    from theoretical_tools import theoretical 
+    
+    mu = partial(mobility, 100)
+    x,y = theoretical(mu, 100, 1, -.01)
+
+
